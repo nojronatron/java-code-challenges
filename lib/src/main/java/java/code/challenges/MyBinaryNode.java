b727178fbf856d242e41f6ce6902bc7cb164d61f
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-07 17:25:12 -0700

    added node and binary tree libraries

---
+++ lib/src/main/java/java/code/challenges/MyBinaryNode.java
@@ -0,0 +1,35 @@
+package java.code.challenges;
+
+public class MyBinaryNode {
+    private int value;
+    private MyBinaryNode left;
+    private MyBinaryNode right;
+
+    public MyBinaryNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public MyBinaryNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(MyBinaryNode left) {
+        this.left = left;
+    }
+
+    public MyBinaryNode getRight() {
+        return right;
+    }
+
+    public void setRight(MyBinaryNode right) {
+        this.right = right;
+    }
+}
---
+++ lib/src/main/java/java/code/challenges/MyBinaryTree.java
@@ -0,0 +1,55 @@
+package java.code.challenges;
+
+public class MyBinaryTree {
+    private MyBinaryNode root = null;
+
+    public MyBinaryTree() {
+        this.root = null;
+    }
+
+    public MyBinaryTree(MyBinaryNode root) {
+        this.root = root;
+    }
+
+    public MyBinaryNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(MyBinaryNode root) {
+        this.root = root;
+    }
+
+    public void preOrder(MyBinaryNode root) {
+        // process root.getValue()
+        if (root.getLeft() != null) {
+            preOrder(root.getLeft());
+        }
+        if (root.getRight() != null) {
+            preOrder(root.getRight());
+        }
+    }
+
+    public void inOrder(MyBinaryNode root) {
+        if (root.getLeft() != null) {
+            inOrder(root.getLeft());
+        }
+        // process root.getValue();
+        if (root.getRight() != null) {
+            inOrder(root.getRight());
+        }
+    }
+
+    public void postOrder(MyBinaryNode root) {
+        if (root.getLeft() != null) {
+            postOrder(root.getLeft());
+        }
+        if (root.getRight() != null) {
+            postOrder(root.getRight());
+        }
+        // process root.getValue();
+    }
+
+    public void breadthFirst(MyBinaryNode root) {
+        // TODO: Implement MyQueue before implementing
+    }
+}
