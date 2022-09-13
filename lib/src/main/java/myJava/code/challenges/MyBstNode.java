e9d1021ddfec09871fc19bbd1139055135035d29
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-13 08:34:25 -0700

    added bstnode to start bst implementation

---
+++ lib/src/main/java/myJava/code/challenges/MyBstNode.java
@@ -0,0 +1,10 @@
+package myJava.code.challenges;
+
+public class MyBstNode {
+    // goals:
+    // 1. create GENERIC binary tree node
+    // 2. pull-in all MyBST functionality and remove MyBST
+    // 3. implement add function (smaller left, larger right)
+    // 4. implement search function
+    // 5. write unit tests for full code-coverage
+}
