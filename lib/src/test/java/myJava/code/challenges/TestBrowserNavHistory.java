612cf03f8ccf4c2e40a87b230c828682a50ac18a
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-28 10:34:10 -0700

    completed writing core unit tests.

--- lib/src/test/java/myJava/code/challenges/TestBrowserNavHistory.java
+++ lib/src/test/java/myJava/code/challenges/TestBrowserNavHistory.java
@@ -11,9 +11,10 @@ public class TestBrowserNavHistory {
     }
     @Test
     public void test_GoAddsItemToEmptyList() {
+        String expectedAddress = "uri-1";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
-        sut.go("uri-1");
+        sut.go(expectedAddress);
         assertEquals(1, sut.getCount());
     }
     @Test
@@ -33,22 +34,77 @@ public class TestBrowserNavHistory {
     }
     @Test
     public void test_ForwardToNullNodeReturnsBlankString() {
-
+        String expectedAddress = "uri-1";
+        String expectedResult = "";
+        var sut = new BrowserNavHistory();
+        assertEquals(0, sut.getCount());
+        sut.go(expectedAddress);
+        assertEquals(1, sut.getCount());
+        assertEquals(expectedAddress, sut.getHead());
+        assertEquals(expectedAddress, sut.getTail());
+        assertEquals(expectedResult, sut.forward());
     }
     @Test
     public void test_ForwardToNodeWithValue() {
-
+        String expectedAddress1 = "uri-1";
+        String expectedAddress2 = "uri-2";
+        var sut = new BrowserNavHistory();
+        assertEquals(0, sut.getCount());
+        sut.go(expectedAddress1);
+        assertEquals(1, sut.getCount());
+        assertTrue(sut.contains(expectedAddress1));
+        sut.go(expectedAddress2);
+        assertEquals(2, sut.getCount());
+        assertEquals(expectedAddress1, sut.back());
+        assertEquals(expectedAddress2, sut.forward());
     }
     @Test
     public void test_BackToHeadNode() {
-
+        String expectedAddress1 = "uri-1";
+        String expectedAddress2 = "uri-2";
+        var sut = new BrowserNavHistory();
+        assertEquals(0, sut.getCount());
+        sut.go(expectedAddress1);
+        assertEquals(1, sut.getCount());
+        assertTrue(sut.contains(expectedAddress1));
+        sut.go(expectedAddress2);
+        assertEquals(2, sut.getCount());
+        assertEquals(expectedAddress1, sut.back());
     }
     @Test
     public void test_BackToNodeWithValue() {
-
+        String expectedAddress1 = "uri-1";
+        String expectedAddress2 = "uri-2";
+        String expectedAddress3 = "uri-3";
+        var sut = new BrowserNavHistory();
+        assertEquals(0, sut.getCount());
+        sut.go(expectedAddress1);
+        assertEquals(1, sut.getCount());
+        assertTrue(sut.contains(expectedAddress1));
+        sut.go(expectedAddress2);
+        assertEquals(2, sut.getCount());
+        assertEquals(expectedAddress1, sut.back());
+        sut.go(expectedAddress3);
+        assertEquals(3, sut.getCount());
+        assertEquals(expectedAddress2, sut.back());
     }
     @Test
-    public void test_GoGoBackGoForwardWorksAsExpected() {
-
+    public void test_GoGoBackForwardWorksAsExpected() {
+        String expectedAddress1 = "uri-1";
+        String expectedAddress2 = "uri-2";
+        String expectedAddress3 = "uri-3";
+        var sut = new BrowserNavHistory();
+        assertEquals(0, sut.getCount());
+        sut.go(expectedAddress1);
+        assertEquals(1, sut.getCount());
+        assertTrue(sut.contains(expectedAddress1));
+        sut.go(expectedAddress2);
+        assertEquals(2, sut.getCount());
+        assertEquals(expectedAddress1, sut.back());
+        sut.go(expectedAddress3);
+        assertEquals(3, sut.getCount());
+        assertEquals(expectedAddress2, sut.back());
+        assertEquals(3, sut.getCount());
+        assertEquals(expectedAddress3, sut.forward());
     }
 }
