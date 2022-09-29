3f23aa1d3f65f2119fe3800d81cbcec9e6c37a4c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-29 14:48:23 -0700

    updated enhanced method unit tests.

--- lib/src/test/java/myJava/code/challenges/TestEnhancedBrowserNavHistory.java
+++ lib/src/test/java/myJava/code/challenges/TestEnhancedBrowserNavHistory.java
@@ -10,8 +10,27 @@ public class TestEnhancedBrowserNavHistory {
         assertNotNull(sut);
     }
     @Test
+    public void test_GoDoesNotThrowWhenGivenBlankOrSpaceInput() {
+        String blank = "";
+        String singleSpace = " ";
+        String twoSpaces = "  ";
+        String elevenSpaces = "           ";
+        var sut = new BrowserNavHistory();
+
+        assertEquals(0, sut.getCount());
+
+        assertDoesNotThrow(()->{
+            sut.enhancedGo(blank);
+            sut.enhancedGo(singleSpace);
+            sut.enhancedGo(twoSpaces);
+            sut.enhancedGo(elevenSpaces);
+        });
+
+        assertEquals(0, sut.getCount());
+    }
+    @Test
     public void test_GoAddsItemToEmptyList() {
-        String expectedAddress = "uri-1";
+        String expectedAddress = "http://u.ri/1";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress);
@@ -19,8 +38,8 @@ public class TestEnhancedBrowserNavHistory {
     }
     @Test
     public void test_GoAddsItemToOccupiedList() {
-        String expectedAddress1 = "uri-1";
-        String expectedAddress2 = "uri-2";
+        String expectedAddress1 = "http://u.ri/1";
+        String expectedAddress2 = "http://u.ri/2";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress1);
@@ -34,20 +53,19 @@ public class TestEnhancedBrowserNavHistory {
     }
     @Test
     public void test_ForwardToNullNodeReturnsBlankString() {
-        String expectedAddress = "uri-1";
-        String expectedResult = "";
+        String expectedAddress = "http://u.ri/1";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress);
         assertEquals(1, sut.getCount());
         assertEquals(expectedAddress, sut.getHead());
         assertEquals(expectedAddress, sut.getTail());
-        assertEquals(expectedResult, sut.enhancedForward());
+        assertEquals(expectedAddress, sut.enhancedForward());
     }
     @Test
     public void test_ForwardToNodeWithValue() {
-        String expectedAddress1 = "uri-1";
-        String expectedAddress2 = "uri-2";
+        String expectedAddress1 = "http://u.ri/1";
+        String expectedAddress2 = "http://u.ri/2";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress1);
@@ -60,8 +78,8 @@ public class TestEnhancedBrowserNavHistory {
     }
     @Test
     public void test_BackToHeadNode() {
-        String expectedAddress1 = "uri-1";
-        String expectedAddress2 = "uri-2";
+        String expectedAddress1 = "http://u.ri/1";
+        String expectedAddress2 = "http://u.ri/2";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress1);
@@ -74,31 +92,31 @@ public class TestEnhancedBrowserNavHistory {
     }
     @Test
     public void test_BackToNodeWithValue() {
-        String expectedAddress1 = "uri-1";
-        String expectedAddress2 = "uri-2";
-        String expectedAddress3 = "uri-3";
+        String expectedAddress1 = "http://u.ri/1";
+        String expectedAddress2 = "http://u.ri/2";
+        String expectedAddress3 = "http://u.ri/3";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
 
-        sut.enhancedGo(expectedAddress1);   //  uri-1 -> null
+        sut.enhancedGo(expectedAddress1);   //  http://u.ri/1 -> null
         assertEquals(1, sut.getCount());
         assertTrue(sut.contains(expectedAddress1));
 
-        sut.enhancedGo(expectedAddress2);   //  uri-2 -> uri-1 -> null
+        sut.enhancedGo(expectedAddress2);   //  http://u.ri/2 -> http://u.ri/1 -> null
         assertEquals(2, sut.getCount());
         assertTrue(sut.contains(expectedAddress1));
         assertTrue(sut.contains(expectedAddress2));
         assertEquals(expectedAddress1, sut.enhancedBack());
 
-        sut.enhancedGo(expectedAddress3);   //  uri-2 -> uri-3 > uri-1 -> null
+        sut.enhancedGo(expectedAddress3);   //  http://u.ri/2 -> http://u.ri/3 > http://u.ri/1 -> null
         assertEquals(3, sut.getCount());
         assertTrue(sut.contains(expectedAddress3));
         assertEquals(expectedAddress1, sut.enhancedBack());
     }
     @Test
     public void test_GoGoBackForwardWorksAsExpected() {
-        String expectedAddress1 = "uri-1";
-        String expectedAddress2 = "uri-2";
+        String expectedAddress1 = "http://u.ri/1";
+        String expectedAddress2 = "http://u.ri/2";
         var sut = new BrowserNavHistory();
         assertEquals(0, sut.getCount());
         sut.enhancedGo(expectedAddress1);
