e174bc716e0552e3cbb60f57d34f78309eaee31f
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-29 14:49:10 -0700

    squashed bugz in enhanced methods, all unit tests passing

--- lib/src/main/java/myJava/code/challenges/BrowserNavHistory.java
+++ lib/src/main/java/myJava/code/challenges/BrowserNavHistory.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 public class BrowserNavHistory {
     private final LinkedList<String> history;
     private Integer currentIdx;
+
     public BrowserNavHistory() {
         this.history = new LinkedList<>();
         this.currentIdx = -1;
@@ -25,30 +26,24 @@ public class BrowserNavHistory {
     /**
      * Adds the address to the front of the list and maintains the existing next address.
      * @param address String
-     * @throws IndexOutOfBoundsException Checked Exception is index is corrup
+     * @throws IndexOutOfBoundsException Checked Exception is index is corrupt
      */
     public void enhancedGo(String address) throws IndexOutOfBoundsException {
-        //  todo: filter-out an all spaces String input
-        if (!address.equals("") && !address.equals(" ")) {
-            if (this.history.contains(address)) {
-                this.currentIdx = this.history.indexOf(address);
+        if (this.isNotBlankInput(address)) {
+            if (this.currentIdx > 0) {
+                String currentAddress = this.history.get(currentIdx);
+                this.history.remove(currentAddress);
+                this.history.addFirst(currentAddress);
                 this.history.remove(address);
             }
-            if (this.currentIdx > -1 && this.history.size() == 2) {
-                this.history.add(address);
-                this.currentIdx = 0;
-            } else if (this.currentIdx > -1 && this.history.size() > 2) {
 
-            }
-            String tempAddress = this.history.get(this.currentIdx);
-                this.currentIdx = 0;
-                this.history.add(tempAddress);
-                this.history.add(address);
-            } else {
-                this.history.add(address);
-                this.currentIdx = 0;
-            }
+            this.history.addFirst(address);
+            this.currentIdx = 0;
         }
+    }
+
+    private boolean isNotBlankInput(String address) {
+        return address.length() >= 11 && !address.matches("\\s+");
     }
 
     /**
@@ -57,7 +52,7 @@ public class BrowserNavHistory {
      */
     public String forward() {
         if (this.currentIdx == 0) {
-            return "";  //  todo: return the first index string instead of nothing
+            return "";
         }
         this.currentIdx--;
         return this.history.get(currentIdx);
@@ -65,7 +60,7 @@ public class BrowserNavHistory {
 
     /**
      * Return the next address 'ahead' of the current location within the history list.
-     * @return
+     * @return String address
      */
     public String enhancedForward() {
         if (this.currentIdx > 0) {
@@ -80,7 +75,7 @@ public class BrowserNavHistory {
 
     /**
      * Returns the address just previous to the currently visited address.
-     * @return
+     * @return String
      */
     public String back() {
         if (this.currentIdx >= this.getCount() - 1) {
@@ -92,7 +87,7 @@ public class BrowserNavHistory {
 
     /**
      * Returns the previous address 'behind' the current location within the history list.
-     * @return
+     * @return String
      */
     public String enhancedBack() {
         if (this.history.size() > this.currentIdx + 1) {
@@ -105,20 +100,35 @@ public class BrowserNavHistory {
         return "";
     }
 
-    // Helper Methods
+    /**
+     * Helper method returns size of internal Linked List.
+     * @return int
+     */
     public int getCount() {
         return history.size();
     }
+
+    /**
+     * Helper method for unit testing.
+     * @param itemToFind String
+     * @return boolean
+     */
     public boolean contains(String itemToFind) {
         return history.contains(itemToFind);
     }
-    public String getItem(String itemToGet) {
-        var itemIdx = history.indexOf("");
-        return history.get(itemIdx);
-    }
+
+    /**
+     * Helper method for unit testing.
+     * @return String
+     */
     public String getHead() {
         return this.history.peekFirst();
     }
+
+    /**
+     * Helper method for unit testing.
+     * @return String
+     */
     public String getTail() {
         return this.history.peekLast();
     }
