0ddc7c0e00abfe6ba825f072ea6eb9122c2e9237
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-30 16:15:42 -0700

    implemented according to pseudocude. StackNode subclassed.

---
+++ lib/src/main/java/myJava/code/models/MyStack.java
@@ -0,0 +1,75 @@
+package myJava.code.models;
+
+public class MyStack {
+    private StackNode top;
+    private StackNode bottom;
+    public MyStack() {
+        this.top = null;
+        this.bottom = null;
+    }
+
+    public void Push(Object data) {
+        StackNode newNode = new StackNode(data);
+        if (this.isEmpty()) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            if (this.bottom == this.top) {
+                this.top = newNode;
+                this.top.next = bottom;
+            }
+        }
+    }
+
+    public Object Pop() {
+        if (this.isEmpty()) {
+            return new NullPointerException("There are no items in this Stack.");
+        } else {
+            StackNode tempNode = this.top;
+            this.top = top.getNext();
+            tempNode.setNext(null);
+            return tempNode.getValue();
+        }
+    }
+
+    public Object Peek() {
+        if (this.isEmpty()) {
+            throw new NullPointerException("There are no items in this Stack.");
+        } else {
+            return this.top.getValue();
+        }
+    }
+
+    public boolean isEmpty() {
+        if (this.top == this.bottom) {
+            if (this.top == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class StackNode {
+        private Object value;
+        private StackNode next;
+        public StackNode(Object data) {
+            this.value = data;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public StackNode getNext() {
+            return next;
+        }
+
+        public void setNext(StackNode next) {
+            this.next = next;
+        }
+    }
+}
