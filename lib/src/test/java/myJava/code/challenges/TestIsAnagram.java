964601eaf25455a7c9f5799482f651500d1c78c8
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-30 21:57:03 -0700

    added to and updated existing tests. All passing.

--- lib/src/test/java/myJava/code/challenges/TestIsAnagram.java
+++ lib/src/test/java/myJava/code/challenges/TestIsAnagram.java
@@ -14,18 +14,48 @@ public class TestIsAnagram {
     public void test_tacoCatReturnsTrue() {
         String anagramInput = "tacocat";
         AnagramExpert sut = new AnagramExpert();
-        assertTrue(sut.isAnagram(anagramInput));
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
     }
     @Test
     public void test_caseSensitiveAnagramReturnsTrue() {
         String anagramInput = "tAcoCaT";
         AnagramExpert sut = new AnagramExpert();
-        assertTrue(sut.isAnagram(anagramInput));
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
     }
     @Test
     public void test_nonAnagramReturnsFalse() {
         String nonAnagramInput = "burgerdog";
         AnagramExpert sut = new AnagramExpert();
-        assertTrue(sut.isAnagram(nonAnagramInput));
+        assertFalse(sut.enhancedIsAnagram(nonAnagramInput));
+    }
+    @Test
+    public void test_verySmallAnagram() {
+        String anagramInput = "a";
+        AnagramExpert sut = new AnagramExpert();
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
+    }
+    @Test
+    public void test_smallAnagram() {
+        String anagramInput = "bb";
+        AnagramExpert sut = new AnagramExpert();
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
+    }
+    @Test
+    public void test_threeCharAnagram() {
+        String anagramInput = "ccc";
+        AnagramExpert sut = new AnagramExpert();
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
+    }
+    @Test
+    public void test_fourCharAnagram() {
+        String anagramInput = "dood";
+        AnagramExpert sut = new AnagramExpert();
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
+    }
+    @Test
+    public void test_eightCharAnagram() {
+        String anagramInput = "grannarg";
+        AnagramExpert sut = new AnagramExpert();
+        assertTrue(sut.enhancedIsAnagram(anagramInput));
     }
 }
