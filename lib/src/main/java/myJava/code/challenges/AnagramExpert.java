9968b5efc6a13e0a287f04829fe7934d0355c740
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-30 21:57:36 -0700

    implemented enhanced method, tested (passing), refactored.

--- lib/src/main/java/myJava/code/challenges/AnagramExpert.java
+++ lib/src/main/java/myJava/code/challenges/AnagramExpert.java
@@ -7,16 +7,44 @@ public class AnagramExpert {
     public AnagramExpert() {
         this.charStack = new MyStack();
     }
+
     public boolean isAnagram(String stringInput) {
         char[] characters = stringInput.toCharArray();
         int arrCount = characters.length;
         int halfLength = arrCount % 2 == 0 ? arrCount / 2 : (arrCount - 1) / 2;
+        // change: allowed iterating up to and including halfLength
         for(int idx=0; idx<halfLength; idx++) {
             this.charStack.push(characters[idx]);
         }
         for(int idx=halfLength; idx <= arrCount-1; idx++) {
             char tempChar = (char)charStack.pop();
             if (Character.toLowerCase(characters[idx]) != Character.toLowerCase(tempChar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean enhancedIsAnagram(String stringInput) {
+        char[] characters = stringInput.toCharArray();
+        int maxIndex = characters.length - 1;
+        int maxLeftIdx = 0;
+
+        if (characters.length % 2 != 0) {
+            maxLeftIdx = (maxIndex / 2) - 1;
+        } else {
+            maxLeftIdx = maxIndex / 2;
+        }
+
+        for (int idx=0; idx<=maxLeftIdx; idx++) {
+            charStack.push(characters[idx]);
+        }
+
+        maxLeftIdx = characters.length % 2 != 0 ? maxLeftIdx + 1 : maxLeftIdx;
+
+        while (!charStack.isEmpty()){
+            maxLeftIdx++;
+            if (Character.toLowerCase((char)charStack.pop()) != Character.toLowerCase(characters[maxLeftIdx])) {
                 return false;
             }
         }
