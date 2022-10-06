55ab44c9a19e3477356c3576d46ce9293db4dd24
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-05 17:20:50 -0700

    framed-in planned unit tests

--- lib/src/test/java/myJava/code/challenges/TestSprinklerWaterUsage.java
+++ lib/src/test/java/myJava/code/challenges/TestSprinklerWaterUsage.java
@@ -1,4 +1,26 @@
 package myJava.code.challenges;
 
+import org.junit.jupiter.api.Test;
+
 public class TestSprinklerWaterUsage {
+    @Test
+    void test_canInstantiateClass() {
+
+    }
+    @Test
+    void test_nullInputReturnsNullOutput() {
+
+    }
+    @Test
+    void test_singleNodeTreeReturnsRootNodeValue() {
+
+    }
+    @Test
+    void test_negativeValuesInTreeIgnored() {
+
+    }
+    @Test
+    void test_exampleInputsReturns249() {
+
+    }
 }
