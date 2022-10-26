2b9098500b319e739337d735ad3cb5fc47c32755
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-26 14:13:48 -0700

    implemented StringBuilder to simplify.

--- lib/src/main/java/myJava/code/challenges/RomanNumeralExpert.java
+++ lib/src/main/java/myJava/code/challenges/RomanNumeralExpert.java
@@ -6,10 +6,9 @@ import java.util.Locale;
 
 public class RomanNumeralExpert {
     public static String toRomanNumerals(String inputNumbers) {
-        String result = "";
 
         if (inputNumbers.length() < 1) {
-            return result;
+            return "";
         }
 
         String[] romanNumerals = {"I", "V", "X", "L", "C", "D", "M", "~V"};
@@ -19,12 +18,14 @@ public class RomanNumeralExpert {
 
         for(String item: inputArr) {
             if (item.equals("-")) {
-                return result;
+                return "";
             }
 
             int intItem = Integer.parseInt(item);
             numberStack.push(intItem);
         }
+
+        StringBuilder result = new StringBuilder();
 
         while (!numberStack.isEmpty()) {
             int currentValue = (int)numberStack.pop();
@@ -32,28 +33,29 @@ public class RomanNumeralExpert {
 
             if (currentValue > 0 && currentValue < 4) {
                 for(int i=0; i < currentValue; i++) {
-                    result = romanNumerals[idx-2] + result;
+                    result.insert(0, romanNumerals[idx-2]);
                 }
             }
 
             if (currentValue == 4) {
-                result = romanNumerals[idx-2] + romanNumerals[idx-1] + result;
+                result.insert(0, romanNumerals[idx-1]);
+                result.insert(0, romanNumerals[idx-2]);
             }
 
             if (currentValue > 4 && currentValue < 9) {
-                String tempRN = romanNumerals[idx-1];
-                currentValue = currentValue - 5;
+                result.insert(0, romanNumerals[idx-1]);
+                currentValue -= 5;
+
                 for (int i=0; i < currentValue; i++) {
-                    result = romanNumerals[idx-2] + result;
+                    result.insert(1, romanNumerals[idx-2]);
                 }
-                result = tempRN + result;
             }
 
             if (currentValue == 9) {
-                result = romanNumerals[idx-2] + romanNumerals[idx] + result;
+                result.insert(0, romanNumerals[idx]).insert(0, romanNumerals[idx-2]);
             }
         }
 
-        return result;
+        return result.toString();
     }
 }
