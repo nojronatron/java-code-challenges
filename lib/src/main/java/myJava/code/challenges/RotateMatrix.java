78d91c2d812a1c264eb5e1babcd31d2f723b9e01
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-11-02 18:50:30 -0700

    framed in Class and Method for sut

---
+++ lib/src/main/java/myJava/code/challenges/RotateMatrix.java
@@ -0,0 +1,9 @@
+package myJava.code.challenges;
+
+public class RotateMatrix {
+    public int[][] rotate(int[][] inputArray) {
+        int[][] result = new int[inputArray.length][inputArray.length];
+
+        return result;
+    }
+}
