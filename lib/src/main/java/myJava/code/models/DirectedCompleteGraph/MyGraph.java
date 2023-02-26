ed133b69a7f6bfa3597f772f0effeefee98911e4
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-02-25 19:46:56 -0800

    implemented recursive solution, probably not good.

--- lib/src/main/java/myJava/code/models/DirectedCompleteGraph/MyGraph.java
+++ lib/src/main/java/myJava/code/models/DirectedCompleteGraph/MyGraph.java
@@ -1,12 +1,15 @@
 package myJava.code.models.DirectedCompleteGraph;
 
 import myJava.code.models.MyQueue;
+import myJava.code.models.MyStack;
 
 import java.util.*;
 
 public class MyGraph<T> {
     private final Hashtable<T, MyGraphNode<T>> adjacencyTable;
     private Set<MyGraphNode<T>> visitedNodes;
+    //private MyStack trackerStack;
+    private Stack<MyGraphEdge<T>> trackerStack;
 
     public MyGraph(MyGraphNode<T> vertex) {
         this.adjacencyTable = new Hashtable<>();
@@ -74,6 +77,42 @@ public class MyGraph<T> {
             }
         }
         return null;
+    }
+
+    public int getWeightBetweenVertices(MyGraphNode<T> root, MyGraphNode<T> target) {
+        if (root.equals(target)) {
+            return 0;
+        }
+        this.trackerStack = new Stack<>();
+        this.visitedNodes = new HashSet<>();
+        this.visitedNodes.add(root);
+        var pathCompleted = false;
+
+        trackPathToTargetDF(root, target, pathCompleted);
+
+        var weightSum = 0;
+        while (!this.trackerStack.isEmpty()) {
+            weightSum += this.trackerStack.pop().getWeight();
+        }
+
+        return weightSum;
+    }
+
+    private void trackPathToTargetDF(MyGraphNode<T> currentVertex, MyGraphNode<T> targetVertex, boolean pathCompleted) {
+        if (!pathCompleted) {
+            for(MyGraphEdge<T> edge: currentVertex.getEdges()) {
+                this.trackerStack.push(edge);
+                if (edge.getNeighbor().equals(targetVertex)) {
+                    pathCompleted = true;
+                    return;
+                }
+                if (!this.visitedNodes.contains(edge.getNeighbor())) {
+                    this.visitedNodes.add(edge.getNeighbor());
+                    trackPathToTargetDF(edge.getNeighbor(), targetVertex, pathCompleted);
+                }
+                this.trackerStack.pop();
+            }
+        }
     }
 
     public ArrayList<T> getVisitedNodes() {
