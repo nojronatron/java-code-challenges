cfef6b69aa7c3899965ec959057e8050073e899b
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-03-18 11:54:38 -0700

    start building out tests.

---
+++ lib/src/test/java/myJava/code/challenges/TestConnectBinaryTrees.java
@@ -0,0 +1,30 @@
+package myJava.code.challenges;
+
+import org.junit.jupiter.api.Test;
+
+public class TestConnectBinaryTrees {
+    @Test
+    public void test_CanInstantiateTrees() {
+
+    }
+    @Test
+    public void test_LeftTreeNullReturnsRightTree() {
+
+    }
+    @Test
+    public void test_RightTreeNullReturnsLeftTree(){
+
+    }
+    @Test
+    public void test_LeftAndRightHaveSixRightMissingRightChild(){
+
+    }
+    @Test
+    public void test_LeftAndRightHaveSixRightMissingLeftChild(){
+
+    }
+    @Test
+    public void test_LeftTreeAddedToLeafInRightTreeWithSeven(){
+
+    }
+}
