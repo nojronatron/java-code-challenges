bba86f48c05597425efa4945aeb7c53137dde155
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-03-31 20:05:38 -0700

    refined code, tests passing.

--- lib/src/main/java/myJava/code/challenges/MergeSorter.java
+++ lib/src/main/java/myJava/code/challenges/MergeSorter.java
@@ -7,22 +7,21 @@ public class MergeSorter {
     }
 
     public void partition(int arrStart, int arrEnd, int[] inputArr) {
+        if (arrStart == arrEnd) {
+            return;
+        }
         int arrStartEndDiff = arrEnd - arrStart;
         int mid = arrStartEndDiff % 2 == 0 ? arrStartEndDiff / 2 : (arrStartEndDiff + 1) / 2;
         mid += arrStart;
         int leftEnd = mid-1;
-        if (leftEnd - arrStart > 1) {
-            partition(arrStart, leftEnd, inputArr);
-        }
-        if (arrEnd - mid > 1) {
-            partition(mid, arrEnd, inputArr);
-        }
-        merge(arrStart, leftEnd, mid, arrEnd, inputArr);
+        partition(arrStart, leftEnd, inputArr);
+        partition(mid, arrEnd, inputArr);
+        merge(arrStart, mid, arrEnd, inputArr);
     }
 
-    public void merge(int leftStart, int leftEnd, int rightStart, int rightEnd, int[] inputArr) {
-        for (int leftIdx=leftStart; leftIdx < rightEnd; leftIdx++){
-            for (int rightIdx=leftIdx+1; rightIdx <= rightEnd; rightIdx++) {
+    public void merge(int leftStart, int midPoint, int rightEnd, int[] inputArr) {
+        for (int leftIdx=leftStart; leftIdx <= rightEnd; leftIdx++){
+            for (int rightIdx = leftIdx+1; rightIdx <= rightEnd; rightIdx++) {
                 if (inputArr[leftIdx] > inputArr[rightIdx]){
                     shift(leftIdx, rightIdx, inputArr);
                 }
