9b285d2caf4c259289d3694ca94c9e77b2ab9ad5
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-05 18:28:22 -0700

    added diamond operator to anagramExpert

--- lib/src/main/java/myJava/code/challenges/AnagramExpert.java
+++ lib/src/main/java/myJava/code/challenges/AnagramExpert.java
@@ -1,37 +1,36 @@
 package myJava.code.challenges;
 
-import myJava.code.models.MyHashtable;
-
 import java.util.Hashtable;
 
 public class AnagramExpert {
-    private final Hashtable<Character, Integer> hashtable;
-    public AnagramExpert() {
-        this.hashtable = new Hashtable();
+  private final Hashtable<Character, Integer> hashtable;
+
+  public AnagramExpert() {
+    this.hashtable = new Hashtable<>();
+  }
+
+  public boolean isAnagram(String word1, String word2) {
+    var firstInput = word1.replaceAll("\\W", "");
+    var secondInput = word2.replaceAll("\\W", "");
+
+    if (firstInput.length() != secondInput.length()) {
+      return false;
     }
 
-    public boolean isAnagram(String word1, String word2) {
-        var firstInput = word1.replaceAll("\\W", "");
-        var secondInput = word2.replaceAll("\\W", "");
+    var inputArr1 = firstInput.toCharArray();
+    var inputArr2 = secondInput.toCharArray();
 
-        if (firstInput.length() != secondInput.length()) {
-            return false;
-        }
-
-        var inputArr1 = firstInput.toCharArray();
-        var inputArr2 = secondInput.toCharArray();
-
-        for (char character : inputArr1) {
-            this.hashtable.put(Character.toLowerCase(character), 1);
-        }
-
-        for (char character : inputArr2) {
-            boolean result = hashtable.containsKey(Character.toLowerCase(character));
-            if (!result) {
-                return false;
-            }
-        }
-
-        return true;
+    for (char character : inputArr1) {
+      this.hashtable.put(Character.toLowerCase(character), 1);
     }
+
+    for (char character : inputArr2) {
+      boolean result = hashtable.containsKey(Character.toLowerCase(character));
+      if (!result) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
