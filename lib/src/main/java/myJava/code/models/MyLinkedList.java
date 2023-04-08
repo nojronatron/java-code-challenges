2e366c09e24b23ec5f5e31ebb85d6dadb93c7147
Jon Rumsey <jon.rumsey@gmail.com>
2023-04-07 17:28:37 -0700

    removed unused import

--- lib/src/main/java/myJava/code/models/MyLinkedList.java
+++ lib/src/main/java/myJava/code/models/MyLinkedList.java
@@ -1,78 +1,77 @@
 package myJava.code.models;
 
-import myJava.code.models.MyLinkedListNode;
-
 public class MyLinkedList {
-    private MyLinkedListNode head;
-    private int count;
-    public MyLinkedList() {
-        this.count = 0;
-    }
+  private MyLinkedListNode head;
+  private int count;
 
-    public MyLinkedListNode getHead() {
-        return this.head;
-    }
+  public MyLinkedList() {
+    this.count = 0;
+  }
 
-    public void setHead(MyLinkedListNode head) {
-        this.head = head;
+  public MyLinkedListNode getHead() {
+    return this.head;
+  }
+
+  public void setHead(MyLinkedListNode head) {
+    this.head = head;
+    this.count++;
+  }
+
+  public int getCount() {
+    return this.count;
+  }
+
+  public boolean includes(int value) {
+    MyLinkedListNode current = this.head;
+    while (current != null) {
+      if (current.getValue() == value) {
+        return true;
+      }
+      current = current.getNext();
+    }
+    return false;
+  }
+
+  public void add(int value) {
+    MyLinkedListNode newNode = new MyLinkedListNode(value);
+    newNode.setValue(value);
+    newNode.setNext(this.head);
+    this.head = newNode;
+    this.count++;
+  }
+
+  public boolean addBefore(int newValue, int valueToAddBefore) {
+    if (this.head == null) {
+      return false;
+    }
+    MyLinkedListNode newNode = new MyLinkedListNode(newValue);
+    if (head.getValue() == valueToAddBefore) {
+      newNode.setNext(this.head);
+      head = newNode;
+      this.count++;
+      return true;
+    }
+    MyLinkedListNode current = this.head;
+    while (current != null) {
+      if (current.getNext().getValue() == valueToAddBefore) {
+        newNode.setNext(current.getNext());
+        current.setNext(newNode);
         this.count++;
+        return true;
+      }
+      current = current.getNext();
     }
+    return false;
+  }
 
-    public int getCount() {
-        return this.count;
+  public String print() {
+    MyLinkedListNode current = this.head;
+    StringBuilder result = new StringBuilder();
+    while (current != null) {
+      result.append("[").append(current.getValue()).append("]->");
+      current = current.getNext();
     }
-
-    public boolean includes(int value) {
-        MyLinkedListNode current = this.head;
-        while(current != null) {
-            if (current.getValue() == value) {
-                return true;
-            }
-            current = current.getNext();
-        }
-        return false;
-    }
-
-    public void add(int value) {
-        MyLinkedListNode newNode = new MyLinkedListNode(value);
-        newNode.setValue(value);
-        newNode.setNext(this.head);
-        this.head = newNode;
-        this.count++;
-    }
-
-    public boolean addBefore(int newValue, int valueToAddBefore) {
-        if (this.head == null) {
-            return false;
-        }
-        MyLinkedListNode newNode = new MyLinkedListNode(newValue);
-        if (head.getValue() == valueToAddBefore) {
-            newNode.setNext(this.head);
-            head = newNode;
-            this.count++;
-            return true;
-        }
-        MyLinkedListNode current = this.head;
-        while (current != null) {
-            if (current.getNext().getValue() == valueToAddBefore) {
-                newNode.setNext(current.getNext());
-                current.setNext(newNode);
-                this.count++;
-                return true;
-            }
-            current = current.getNext();
-        }
-        return false;
-    }
-
-    public String print() {
-        MyLinkedListNode current = this.head;
-        StringBuilder result = new StringBuilder();
-        while (current != null) {
-            result.append("[").append(current.getValue()).append("]->");
-            current = current.getNext();
-        }
-        result.append("NULL");
-        return result.toString();
-    }
+    result.append("NULL");
+    return result.toString();
+  }
 }
